c962ef7fa4a073e7b6655d3a8d48a5c7f51c7700
Dhushyanth-96 <88694692+Dhushyanth-96@users.noreply.github.com>
2022-05-23 19:12:23 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { AuthgaurdGuard } from './service/authgaurd.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'patientlist',component:PatientListComponent,canActivate:[AuthgaurdGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
