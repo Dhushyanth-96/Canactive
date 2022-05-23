4d5890a367977218df4ee5bc1e26af01db5399d4
Dhushyanth-96 <88694692+Dhushyanth-96@users.noreply.github.com>
2022-05-23 19:18:41 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { AuthgaurdGuard } from './service/authgaurd.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule
+    
   ],
-  providers: [],
+  providers: [AuthgaurdGuard ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
